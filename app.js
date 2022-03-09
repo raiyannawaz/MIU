19d3dfe24517f311b9c6702a4c1abdfbc08fa716
raiyannawaz <raiyannawaz95@gmail.com>
2022-03-09 12:15:29 +0530

    Demo Project

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 80;
+const hostname = '127.0.0.1';
+const path = require('path')
+
+const viewPath = path.join(__dirname, 'views')
+
+app.set('view engine', 'pug')
+app.set('views', viewPath)
+
+app.get('/', (req, res)=>{
+    res.render('home', {
+        title: "Dance Web"
+    })
+})
+
+app.get('/contact', (req, res)=>{
+    res.render('contact', {
+        title: "Dance Web"
+    })
+})
+
+app.listen(port, hostname, ()=>{
+    console.log(`Listening at http://${hostname}:${port}`)
+})
